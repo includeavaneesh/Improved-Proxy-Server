1c03f4a890a14e784e9f58d50f88f16e13b8ea42
includeavaneesh <avaneeshkanshi5@gmail.com>
2021-10-03 13:31:06 +0530

    basic reverse proxy setup 1

--- reverse-proxy/server.js
+++ reverse-proxy/server.js
@@ -1,10 +1,29 @@
 const express = require('express');
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
-const app = express();
+const options = {
+    target: 'http://www.example.org',
+    changeOrigin : true,
+    ws : true,
+    pathRewrite: {
+        '^/api/old-path': '/api/new-path', // rewrite path
+        '^/api/remove/path': '/path', // remove base path
+    },
 
-app.use('/api', createProxyMiddleware({ target: 'http://www.example.org', changeOrigin: true }));
-var server = app.listen(3000,function(){
+    router: {
+        // when request.headers.host == 'dev.localhost:3000',
+        // override target 'http://www.example.org' to 'http://localhost:8000'
+        'dev.localhost:3000': 'http://localhost:8000',
+    },
+}
+const exampleProxy = createProxyMiddleware(options);
+
+const app = express();
+app.use('/api', exampleProxy);
+
+// app.use('/api', createProxyMiddleware({ target: 'http://www.example.org', changeOrigin: true }));
+
+var server = app.listen(3000, function(){
     var host = server.address().address
     var port = server.address().port
    
